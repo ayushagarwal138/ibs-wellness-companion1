5cad551c2034369407dc010d053eeebbb6910e49
Ayush Agarwal <agarwalayush318@gmail.com>
2026-03-08 23:34:42 +0530

    fix: add pages/_document and custom error pages

---
+++ frontend/src/pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>500 - Server Error</h1>
+      <a href="/">Go Home</a>
+    </div>
+  )
+}
